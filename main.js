2adde2bc942e04d1a05a4aaaf4fb35f384cc270b
Anna <annadalex@icloud.com>
2024-01-30 16:52:05 +0100

    creazione array di oggetti

--- main.js
+++ main.js
@@ -3,6 +3,28 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
+      newTask:{
+        text:``,
+        done: false
+      }
+      tasks:[
+        {
+          text:'fare i compiti',
+          done: true
+        },
+        {
+          text:'fare la spesa',
+          done: true
+        },
+        {
+          text:'fare la lavatrice',
+          done: true
+        },
+        {
+          text:'porta a spasso il cane',
+          done: true
+        },
+      ]
     } 
   },
   created(){
